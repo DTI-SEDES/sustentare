cbe4aca8123d405c8e2cc7f0f2419a412ab86344
DTI-SEDES <dionatan-aristimunha@social.rs.gov.br>
2025-10-20 11:15:00 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sustentare",
-    version="1.0.0",
+    version="1.1.0",
     description="Programa Sustentare - DTI/SEDES",
     packages=find_packages(),
     install_requires=[
